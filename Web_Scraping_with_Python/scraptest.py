944178e7f73451efdbe0bb407a62827835b892f3
mjiang <mjiang@localhost.localdomain>
2020-05-23 09:26:41 +0800

    Exceptions handling for url and http(s)

--- Web_Scraping_with_Python/scraptest.py
+++ Web_Scraping_with_Python/scraptest.py
@@ -5,19 +5,41 @@
 
 # from urllib import urlopen
 from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.baidu.com/")
-bsObj = BeautifulSoup(html.read(), features="html.parser")
+# handle exception when urlopen fails
+try:
+	html = urlopen("http://www.baidu.com/")
+	# html = urlopen("http://mengmajiang.com/")
+except URLError as e:
+	print(e)
+	'''
+    you can do something here as below:
+	1). break the function
+	2). return null
+	3). execute some other funcs 
+	'''
+	# break
+	# return
+	# ...
+except HTTPError as e:
+	print(e)
+	# same as the last exception
+else:
+	'''
+	continue the function in this section
+	'''
+	# print(html.read())
+	bsObj = BeautifulSoup(html.read(), features="html.parser")
 
-# print(html.read())
 
-# Print the first element which beautiful soup find
-print(bsObj.head)
-print(bsObj.span)
-print(bsObj.script)
+	# Print the first element which beautiful soup find
+	print(bsObj.head)
+	print(bsObj.span)
+	print(bsObj.script)
 
-# Print the node of specified element
-print(bsObj.head.script)
-print(bsObj.body.div)
+	# Print the node of specified element
+	print(bsObj.head.script)
+	print(bsObj.body.div)
 
