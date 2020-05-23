42e8b96bacce556f6dc53fdb3f23a36977fa7d45
mjiang <mjiang@localhost.localdomain>
2020-05-23 08:54:09 +0800

    Fix some runtime error for url lib and test with bs with simple cases

--- Web_Scraping_with_Python/scraptest.py
+++ Web_Scraping_with_Python/scraptest.py
@@ -1,9 +1,23 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+#coding: utf-8
 # urllib.request has been remove in python3
 # We use urllib directly
-from urllib import urlopen
+
+# from urllib import urlopen
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen("http://www.baidu.com/")
-print(html.read())
+bsObj = BeautifulSoup(html.read(), features="html.parser")
+
+# print(html.read())
+
+# Print the first element which beautiful soup find
+print(bsObj.head)
+print(bsObj.span)
+print(bsObj.script)
+
+# Print the node of specified element
+print(bsObj.head.script)
+print(bsObj.body.div)
 
